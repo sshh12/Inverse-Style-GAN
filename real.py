efad2689f712537959d8735876870b3622c15349
Shrivu Shankar <shrivu1122@gmail.com>
2019-10-19 11:10:42 -0500

    🧪: Added Facial Features Layer

--- real.py
+++ real.py
@@ -1,3 +1,6 @@
+"""
+TODO
+"""
 import cv2
 import time
 import click
@@ -13,9 +16,12 @@ import sys; sys.path.append('../stylegan')
 from dnnlib import tflib; tflib.init_tf()
 from dnnlib import util
 
+from keras_vggface.vggface import VGGFace
+from keras.models import Model as KerasModel
+from keras.layers import Lambda as KerasLambda
+
 import config
 FACE_MODEL_URL = "https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ"
-
 
 with util.open_url(FACE_MODEL_URL, cache_dir=config.cache_dir) as f:
     _G, _D, Gs = pickle.load(f)
@@ -42,16 +48,42 @@ class GANLayer(tf.keras.layers.Layer):
     def call(self, input_):
         return self.Gs.get_output_for(input_, None, is_validation=True, randomize_noise=True)
 
+class FaceFeatures(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        kwargs['trainable'] = False
+        super(FaceFeatures, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        vggface_model = VGGFace(model='resnet50', include_top=True)
+        feat_out = vggface_model.layers[-2].output
+        self.model = KerasModel(vggface_model.input, feat_out)
+
+    def call(self, input_):
+        input_ = tf.transpose(input_, [0, 2, 3, 1])
+        input_resized = tf.image.resize( 
+            input_, 
+            (224, 224), 
+            method=tf.image.ResizeMethod.BICUBIC
+        )
+        return self.model(input_resized)
+
+ref_model = tf.keras.Sequential([
+  FaceFeatures()
+])
+ref_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss=tf.keras.losses.MSE)
+my_vec = ref_model.predict(me)
+
 model = tf.keras.Sequential([
   DummyLayer(input_shape=(512,)),
-  GANLayer()
+  GANLayer(),
+  FaceFeatures()
 ])
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
               loss=tf.keras.losses.MSE)
 
-hist = model.fit(np.zeros((1, 512)), me, epochs=1000)
-
-vec = model.layers[0].get_weights()[0]
-
-np.save('test-real.npy', vec)
+for i in range(10000):
+    hist = model.fit(np.empty((1, 512)), my_vec, epochs=500)
+    loss = hist.history['loss'][-1]
+    vec = model.layers[0].get_weights()[0]
+    np.save('out\\test-real-{:.3f}.npy'.format(loss), vec)
